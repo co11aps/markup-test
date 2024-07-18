c44723bdd91dcd38c6bbb0bf7079f4d418733278
co11aps <143988142+co11aps@users.noreply.github.com>
2024-07-18 18:05:45 +0800

    11

--- main.js
+++ main.js
@@ -20,10 +20,3 @@ import "./src/css/styles.css";
 
 // setupCounter(document.querySelector("#counter"));
 // define a function that sets min-height of my-element to window.innerHeight:
-
-const documentHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
-};
-window.addEventListener("resize", documentHeight);
-documentHeight();
